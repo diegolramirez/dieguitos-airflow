c278b9f4f80d41f7a939c2250e7d8704049be8dc
Diegol Ramírez-Milano <diegolramirezmilano@gmail.com>
2023-02-19 22:58:06 +0100

    modified dag to prevent infinite runs

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -4,25 +4,18 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2023, 2, 20),
-    "email": ["airflow@example.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-}
-
-dag = DAG("hello_world", default_args=default_args, schedule_interval=timedelta(days=1))
-
 
 def print_hello():
     print("Hello, World!")
 
 
-with dag:
+with DAG(
+    dag_id="hello_world",
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["hello"],
+) as dag:
     start = DummyOperator(task_id="start")
     hello_world = PythonOperator(
         task_id="print_hello", python_callable=print_hello, dag=dag

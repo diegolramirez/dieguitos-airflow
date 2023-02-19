b5972e8b71d18015c0a2b891c0a44be8c9bf5095
Diegol Ramírez-Milano <diegolramirezmilano@gmail.com>
2023-02-19 23:06:52 +0100

    changed volumes to absolute paths

--- run-airflow.sh
+++ run-airflow.sh
@@ -5,7 +5,7 @@ docker build -t dieguitos-airflow .
 
 # Start a container with the image
 docker run --env-file ./.env -p 8080:8080 \
-    -v ./dags:/opt/airflow/dags \
-    -v ./logs:/opt/airflow/logs \
-    -v ./plugins:/opt/airflow/plugins \
+    -v "/$(pwd)/dags:/opt/airflow/dags" \
+    -v "/$(pwd)/logs:/opt/airflow/logs" \
+    -v "/$(pwd)/plugins:/opt/airflow/plugins" \
     dieguitos-airflow
